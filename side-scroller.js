47359d0af48b0b76f7ad75fc926ddcf8038f7727
Joseph Cosentino <jcosentino11@gmail.com>
2014-11-04 09:47:18 -0500

    added base enemy, jumping enemy

--- side-scroller.js
+++ side-scroller.js
@@ -21,12 +21,45 @@ $(function() {
                 sprite: "player",
                 type: Q.SPRITE_PLAYER,
                 points: [ [ -16, 44], [ -23, 35 ], [-23,-48], [23,-48], [23, 35 ], [ 16, 44 ]],
-                jumpSpeed: -400
+                jumpSpeed: -400,
+                attack: 5,
+                health: Q.state.get('health')
             });
             this.add('2d, platformerControls, animation, tween');
+
+            this.on('bump.bottom',this,'stomp');
+            this.on('bump.top',this,'attacked');
+            this.on('bump.left',this,'attacked');
+            this.on('bump.right',this,'attacked');
         },
 
-        step: function(dt) {
+        attacked: function(col){
+            if(col.obj instanceof Q.Enemy){
+                console.log(this.p.health)
+                this.p.health -= col.obj.p.attack;
+                Q.state.set('health',this.p.health);
+
+                if(this.p.health <= 0){
+                    //player died
+                    Q.stageScene("game");
+                }
+
+                //add bounceback?
+                // this.p.vx = col.impact * col.normalX;
+                // this.p.vy = col.impact * col.normalY;
+            }
+        },
+
+        stomp: function(col){
+
+            if(col.obj instanceof Q.Enemy){  
+                col.obj.trigger('attack',this.p.attack);
+                this.p.vy = -200;
+            }
+            
+        },
+
+        move: function(dt){
             if(this.p.vx > 0) {
                 if(this.p.landed > 0) {
                     this.play("walk_right");
@@ -44,14 +77,131 @@ $(function() {
             } else {
                 this.play("stand_" + this.p.direction);
             }
+        },
+
+        step: function(dt) {
+            this.move(dt);
+        }
+    });
+
+    Q.Sprite.extend("Enemy", {
+        init: function(p) {
+            this._super(p, {
+                sheet: "player",
+                sprite: "enemy",
+                scale: 0.6,
+                health: 5,
+                points: [ [ -16, 44], [ -23, 35 ], [-23,-48], [23,-48], [23, 35 ], [ 16, 44 ]],
+                jumpSpeed: -200,
+                attack: 1,
+                direction: "right"
+            });
+
+            this.add('2d, animation, tween');
+
+            this.on('attack',this,'attacked');
+        },
+
+        move: function(dt){
+            if(this.p.vx > 0) {
+                if(this.p.landed > 0) {
+                    this.play("walk_right");
+                } else {
+                    this.play("jump_right");
+                }
+                this.p.direction = "right";
+            } else if(this.p.vx < 0) {
+                if(this.p.landed > 0) {
+                    this.play("walk_left");
+                } else {
+                    this.play("jump_left");
+                }
+                this.p.direction = "left";
+            } else {
+                this.play("stand_" + this.p.direction);
+            }
+        },
+
+        attacked: function(attack){
+            this.p.health -= attack;
+            if(this.p.health <= 0){
+                this.destroy();
+            }
+        },
+
+        step: function(dt){
+            this.move(dt);
+        }
+    });
+
+    Q.Enemy.extend("Jumper", {
+        init: function(p) {
+            this._super(p);
+            this.p.resetCount = 50;
+            this.p.count = this.p.resetCount;
+            this.p.jumpSpeed = -400;    
+        },
+
+        jump: function(){
+            this.p.vy = this.p.jumpSpeed;
+        },
+
+        step: function(dt){
+            this._super();
+            if(this.p.count === 0){
+                this.jump();
+                this.p.count = this.p.resetCount;
+            }
+
+            this.p.count--;
+        }
+    });
+
+    Q.Enemy.extend("Roamer", {
+        init: function(p) {
+            this._super(p);
+        },
+
+        step: function(dt){
+
+        }
+    })
+
+
+    Q.UI.Text.extend("Health",{
+        init: function(p){
+            this._super({
+                label: "health: " + Q.state.get("health"),
+                x: 70,
+                y: 20,
+                size: 18,
+                family: "Tahoma",
+                color: "black"
+            });
+
+            Q.state.on("change.health",this,"health");
+        },
+
+        health: function(health){
+            this.p.label = "health: " + health;
         }
     });
 
     ////Scenes///////////////////////////////////////////////////
 
+    Q.scene('hud',function(stage){
+        stage.insert(new Q.Health());
+    });
+
     Q.scene('game',function(stage) {
+        Q.state.reset({health: 20});
+        Q.stageScene('hud',1);
         Q.stageTMX("level1.tmx",stage);
         stage.add("viewport").follow(Q("Player").first());
+
+        var enemy = stage.insert(new Q.Jumper({x: 800, y: 200}));
+
+        
 
         Q.debug = true;
         //Q.debugFill = true;
@@ -68,10 +218,23 @@ $(function() {
             jump_left: { frames:  [13], rate: 1/10, flip: "x" },
             stand_right: { frames:[14], rate: 1/10, flip: false },
             stand_left: { frames: [14], rate: 1/10, flip:"x" },
-            duck_right: { frames: [15], rate: 1/10, flip: false },
-            duck_left: { frames:  [15], rate: 1/10, flip: "x" },
-            climb: { frames:  [16, 17], rate: 1/3, flip: false }
+            // duck_right: { frames: [15], rate: 1/10, flip: false },
+            // duck_left: { frames:  [15], rate: 1/10, flip: "x" },
+            // climb: { frames:  [16, 17], rate: 1/3, flip: false }
         });
+
+        Q.animations("enemy", {
+            walk_right: { frames: [0,1,2,3,4,5,6,7,8,9,10], rate: 1/15, flip: false, loop: true },
+            walk_left: { frames:  [0,1,2,3,4,5,6,7,8,9,10], rate: 1/15, flip:"x", loop: true },
+            jump_right: { frames: [13], rate: 1/10, flip: false },
+            jump_left: { frames:  [13], rate: 1/10, flip: "x" },
+            stand_right: { frames:[14], rate: 1/10, flip: false },
+            stand_left: { frames: [14], rate: 1/10, flip:"x" },
+            // duck_right: { frames: [15], rate: 1/10, flip: false },
+            // duck_left: { frames:  [15], rate: 1/10, flip: "x" },
+            // climb: { frames:  [16, 17], rate: 1/3, flip: false }
+        });
+
         Q.stageScene('game');
     });
 });
